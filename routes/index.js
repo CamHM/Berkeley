88e1f5993f048dfdf24c46cdfe744fc2b329a941
CamHM <cami.andre469@gmail.com>
2019-06-27 03:03:11 -0500

    actualización - petición y recepción de hora

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,8 @@ router.get('/', function(req, res) {
 
 module.exports = function(io) {
     const COUNTRY = 'es-CO';
+    let sockets = new Map();
+
     viewSocket = io.of('/view');
     viewSocket.on('connection', (socket) => {
        console.log('New view connection');
@@ -20,16 +22,17 @@ module.exports = function(io) {
         console.log('New hour connection');
         socket.join('hour room');
         console.log('Socket add it to hour room');
+
         setInterval(() => {
             hourSocket.to('hour room').emit('time', 'send Time');
-            socket.on('send', (message) => {
-                console.log(`This time came: ${new Date(message.hour).toLocaleTimeString(COUNTRY)}`)
-            });
-        }, 15000);
+        }, 20000);
+
+        socket.on('send', (message) => {
+            sockets.set(socket, message.hour);
+            console.log(sockets);
+            console.log(`This time came: ${new Date(message.hour).toLocaleTimeString(COUNTRY)}`)
+        });
     });
-
-    let sockets = new Map();
-
 
     return router;
 };
