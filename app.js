a90161db41dd1dbbe52906c18fe3947de94d0c0e
Camila Andrea Herrera Melo <cami.andre469@gmail.com>
2019-07-10 11:29:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const app = express();
@@ -11,9 +9,6 @@ const indexRouter = require('./routes/index' )(io);
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
